25930dad846ab76a32d31d37528f3f87e2f3bde6
Hamilton Souza <61994118+hamiltonsouza@users.noreply.github.com>
2024-01-28 22:40:30 -0300

    Add operation function and result variable

--- javascript.js
+++ javascript.js
@@ -1,19 +1,43 @@
-function add(firstAddend,secondAddend) {
-    return firstAddend + secondAddend;
-};
-
-function subtract(minuend,subtrahend) {
-    return minuend - subtrahend;
-};
-
-function multiply(multiplier,multiplicand) {
-    return multiplier * multiplicand;
-};
-
-function divide(dividend,divisor) {
-    return dividend,divisor;
-};
-
+let result = 0;
 let firstTerm = 0;
 let operator = 0;
 let secondTerm = 0;
+
+function add(firstAddend,secondAddend) {
+    result = firstAddend + secondAddend;
+    return result;
+};
+
+function subtract(minuend,subtrahend) {
+    result = minuend - subtrahend;
+    return result;
+};
+
+function multiply(multiplier,multiplicand) {
+    result = multiplier * multiplicand;
+    return result;
+};
+
+function divide(dividend,divisor) {
+    result = dividend / divisor;
+    return result;
+};
+
+function operation(firstTerm,operator,secondTerm) {
+    switch(operator) {
+        case '+':
+            add(firstTerm,secondTerm);
+            return result;
+        case '-':
+            subtract(firstTerm,secondTerm);
+            return result;
+        case '*':
+            multiply(firstTerm,secondTerm);
+            return result;
+        case '/':
+            divide(firstTerm,secondTerm);
+            return result;
+        default:
+            return result;
+    }
+};
